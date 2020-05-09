4da7b9f7e1ee67843da55cb1f80795441ae4bd6c
thiago Siqueira <thiagofelipesiqueira@gmail.com>
2020-05-09 02:20:25 -0300

    Creation of functions to insert and delete data through the API with ReactJS

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      title: "My three Repositories",
+      url: "Github",
+      techs: ["C#", "Golang", "F#"],
+
+    });
+
+    setRepositories([...repositories, response.data]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+
+      await api.delete(`repositories/${id}`);
+      setRepositories(repositories.filter(repositorie => repositorie.id !== id));
+
+    } catch (err) {
+        alert('Erro ao deletar, tente novamente.');
+    }
+    
   }
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    return (
+      <div>
+        
+        <ul data-testid="repository-list">
+          
+          {repositories.map(repositorie => (
+          <li key={repositorie.id}>
+            {repositorie.title}
+    
+            <button onClick={() => handleRemoveRepository(`${repositorie.id}`)}>
+              Remover
+            </button>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
-}
+          </li>
+          ))}
+        </ul>
+  
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
+    );
+  }
 
 export default App;
